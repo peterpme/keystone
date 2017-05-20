f664d8534c5cfcff284264bcb29eea794f1be404
Peter Piekarczyk <peterpiekarczyk@gmail.com>
2017-05-20 14:39:33 -0500

    go back to previous bundle

--- admin/server/app/createStaticRouter.js
+++ admin/server/app/createStaticRouter.js
@@ -19,12 +19,20 @@ function buildFieldTypesStream (fieldTypes) {
 		src += 'exports.' + i + 's = {\n';
 		types.forEach(function (type) {
 			if (typeof fieldTypes[type] !== 'string') return;
-			src += type + ': require("../../fields/types/' + type + '/' + fieldTypes[type] + i + '"),\n';
+			src
+        += type
+        + ': require("../../fields/types/'
+        + type
+        + '/'
+        + fieldTypes[type]
+        + i
+        + '"),\n';
 		});
-		// Append ID and Unrecognised column types
+    // Append ID and Unrecognised column types
 		if (i === 'Column') {
 			src += 'id: require("../../fields/components/columns/IdColumn"),\n';
-			src += '__unrecognised__: require("../../fields/components/columns/InvalidColumn"),\n';
+			src
+        += '__unrecognised__: require("../../fields/components/columns/InvalidColumn"),\n';
 		}
 
 		src += '};\n';
@@ -37,7 +45,7 @@ module.exports = function createStaticRouter (keystone) {
 	var writeToDisk = keystone.get('cache admin bundles');
 	var router = express.Router();
 
-	/* Prepare browserify bundles */
+  /* Prepare browserify bundles */
 	var bundles = {
 		fields: browserify({
 			stream: buildFieldTypesStream(keystone.fieldTypes),
@@ -58,17 +66,26 @@ module.exports = function createStaticRouter (keystone) {
 		}),
 	};
 
-	// prebuild static resources on the next tick in keystone dev mode; this
-	// improves first-request performance but delays server start
-	if (process.env.KEYSTONE_DEV === 'true' || process.env.KEYSTONE_PREBUILD_ADMIN === 'true') {
+  // prebuild static resources on the next tick in keystone dev mode; this
+  // improves first-request performance but delays server start
+	if (
+    process.env.KEYSTONE_DEV === 'true'
+    || process.env.KEYSTONE_PREBUILD_ADMIN === 'true'
+  ) {
 		bundles.fields.build();
 		bundles.signin.build();
 		bundles.admin.build();
 	}
 
-	/* Prepare LESS options */
-	var elementalPath = path.join(path.dirname(require.resolve('elemental')), '..');
-	var reactSelectPath = path.join(path.dirname(require.resolve('react-select')), '..');
+  /* Prepare LESS options */
+	var elementalPath = path.join(
+    path.dirname(require.resolve('elemental')),
+    '..'
+  );
+	var reactSelectPath = path.join(
+    path.dirname(require.resolve('react-select')),
+    '..'
+  );
 	var customStylesPath = keystone.getPath('adminui custom styles') || '';
 
 	var lessOptions = {
@@ -82,12 +99,24 @@ module.exports = function createStaticRouter (keystone) {
 		},
 	};
 
-	/* Configure router */
-	router.use('/styles', less(path.resolve(__dirname + '/../../public/styles'), lessOptions));
-	router.use('/styles/fonts', express.static(path.resolve(__dirname + '/../../public/js/lib/tinymce/skins/keystone/fonts')));
-    router.get('/admin/bundles/js/' + keystoneHash + '-FieldTypes', bundles.fields.serve);
-    router.get('/admin/bundles/js/' + keystoneHash + '-Signin/index.js', bundles.signin.serve);
-    router.get('/admin/bundles/js/' + keystoneHash + '-App/index.js', bundles.admin.serve);
+	console.log('keystonehash', keystoneHash);
+
+  /* Configure router */
+	router.use(
+    '/styles',
+    less(path.resolve(__dirname + '/../../public/styles'), lessOptions)
+  );
+	router.use(
+    '/styles/fonts',
+    express.static(
+      path.resolve(
+        __dirname + '/../../public/js/lib/tinymce/skins/keystone/fonts'
+      )
+    )
+  );
+	router.get('/js/fields.js', bundles.fields.serve);
+	router.get('/js/signin.js', bundles.signin.serve);
+	router.get('/js/admin.js', bundles.admin.serve);
 	router.use(express.static(path.resolve(__dirname + '/../../public')));
 
 	return router;
